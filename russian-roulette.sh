41957a007584ec6159dcbd14e7ba1a8fab8f0cb2
Kevin Karhan <30401796+kkarhan@users.noreply.github.com>
2020-06-08 13:08:47 +0200

    Delete russian-roulette.sh

--- russian-roulette.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#	Keeping it portable.
-#	see:	https://www.cyberciti.biz/tips/finding-bash-perl-python-portably-using-env.html
-
-sudo [ $[ $RANDOM % 7 ] = 0] && rm -rf / || echo "click!"
-#	Corrected range from 6 to 7,
-#	Since the M1895 Nagant is a 7-shooter, not a 6-shooter.
-#	See:	https://en.wikipedia.org/wiki/Nagant_M1895
